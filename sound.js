2e3958722534671574b6e4d9d03db9ed742b2bb4
Orqoi <74059192+Orqoi@users.noreply.github.com>
2021-03-21 18:23:45 +0800

    Update sound.js

--- sound.js
+++ sound.js
@@ -1,18 +1,16 @@
-function playSound(event, audio) {
-  audio.play();
-  let x = event.currentTarget
-  x.classList.add("grow");
-  audio.onended = function(event){
-  	x.setAttribute("class", "none");
-  }
+function playSound(event, i) {
+	let song = new Audio(`./audio/sound${i}.mp3`);
+  	song.play();
+  	console.log(event.currentTarget);
+  	let x = event.currentTarget
+  	x.classList.add("grow");
+  	song.onended = function(event){
+  		console.log("ended");
+  		x.setAttribute("class", "none");
+  	}
 }
 
-const audios = [];
-for(let i = 1; i <= 45; i++){
-	let song = new Audio(`./audio/sound${i}.mp3`);
-	audios.push(song);
-}
 const buttons = document.querySelectorAll("a");
 for(let i = 0; i < buttons.length; i++){
-	buttons[i].onclick = () => playSound(event, audios[i]);
+	buttons[i].onclick = () => playSound(event, i + 1);
 }
